39d06d378682f6ac798ca9242dabeb384b1e6892
Maegan Phan <maegyoung@gmail.com>
2023-03-06 14:29:32 -0600

    book4-chapter4 complete. list with events and events by user added to reports.

--- db.sql
+++ db.sql
@@ -1,9 +1,14 @@
-SELECT g.id, u.first_name || ' ' || u.last_name AS full_name, s.id,
-    s.name,
-    s.game_type_id,
-    s.max_players
+SELECT g.id,
+    u.first_name || ' ' || u.last_name AS full_name, 
+    g.user_id,
+    e.id,
+    e.date,
+    e.time,
+    s.name AS game_name
 FROM levelupapi_gamer g
 JOIN auth_user u
     ON g.user_id = u.id
+JOIN levelupapi_event e
+    ON e.organizer_id = g.id
 JOIN levelupapi_game s
-    ON s.gamer_id = g.user_id
+    ON e.game_id = s.id
