c05687ef3344993d787a8c3fdf7abaca7f74f661
Maegan Phan <maegyoung@gmail.com>
2023-03-06 12:09:52 -0600

    minor edits to sql file that TBH shouldnt even be commited but why not?

--- db.sql
+++ db.sql
@@ -1,16 +1,3 @@
-SELECT
-    g.id,
-    u.first_name |''| u.last_name AS 'full_name',
-    s.id,
-    s.name,
-    s.game_type_id,
-    s.max_players,
-FROM levelupapi_gamer AS g
-JOIN auth_user AS u
-    ON levelupapi_g.user_id = u.id
-JOIN levelupapi_game AS s
-    ON s.gamer_id = g.user_id
-
 SELECT g.id, u.first_name || ' ' || u.last_name AS full_name, s.id,
     s.name,
     s.game_type_id,
